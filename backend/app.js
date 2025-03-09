7f8cc20109a57d6bdd2aae993bf2275e0658bf91
AyushiMaithani <ayushimaithani04@gmail.com>
2025-03-09 19:18:49 +0530

    add payment gateway

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,7 @@ app.get('/', (req, res) => {
 app.use('/api/user', require('./routes/userRoute'));
 app.use('/api/order', require('./routes/orderRoute'));
 app.use('/api/table', require('./routes/tableRoute'));
+app.use('/api/payment', require('./routes/paymentRoute'));
 
 //global error handler(to be at last)
 app.use(globalErrorHandler)
--- frontend/src/https/index.js
+++ frontend/src/https/index.js
@@ -9,10 +9,16 @@ const api=axios.create({
     }
 });
 
-//api endpoints
+//auth endpoints
 export const login=(data)=>api.post('/api/user/login',data);
 export const register=(data)=>api.post('/api/user/register',data);
 export const getUserData=()=>api.get('/api/user');
 export const logout=()=>api.post('/api/user/logout');
+
+//table endpoints
 export const addTable=(data)=>api.post('/api/table',data);
 export const getTables=()=>api.get('/api/table');
+
+//payment endpoints
+export const createOrderRazorpay=(data)=>api.post('/api/payment/create-order',data);
+export const verifyPaymentRazorpay = (data) =>api.post("/api/payment/verify-payment", data);
