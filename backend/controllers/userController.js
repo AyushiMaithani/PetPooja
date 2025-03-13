99ae114a09381b7e8b7f5841173d1660efb0c167
AyushiMaithani <ayushimaithani04@gmail.com>
2025-03-13 10:30:21 +0530

    fix typo

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,7 +64,7 @@ const login=async(req,res,next)=>{
             maxAge:1000*60*60*24*30,
             httpOnly:true,
             secure:true,
-            sameSite:"none"
+            sameSite:"None"
         });
 
         res.status(200).json({
